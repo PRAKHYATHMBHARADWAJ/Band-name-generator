e4f4c3b05c2de46e14455f6d90acf30f384369c5
PRAKHYATH M BHARADWAJ <prakhyath.m2005@gmail.com>
2025-05-11 17:29:39 +0530

    Add files via upload

---
+++ Project 1.py
@@ -0,0 +1,4 @@
+print("welcome to band name generator!!")
+name1=(input("whats the name of the city you grew up in??\n"))
+name2=(input("whats ur pet name??\n"))
+print("your band name could be:"  + name1 + " " + name2)
